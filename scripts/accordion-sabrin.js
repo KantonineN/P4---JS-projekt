0bc4d4353fc0c1bd7d806474e5c8d2ba9c1aa25b
KantonineN <nbg.100382@gmail.com>
2024-08-28 10:30:10 +0200

    Update accordion-sabrin.js
    Packed accordion in a function

--- scripts/accordion-sabrin.js
+++ scripts/accordion-sabrin.js
@@ -1,18 +1,22 @@
-const acc = document.getElementsByClassName("accordion");
+function Accordion() {
+    const acc = document.getElementsByClassName("accordion");
 
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        
-        const panel = this.nextElementSibling;
-        const arrow = this.querySelector(".arrow");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            
+            const panel = this.nextElementSibling;
+            const arrow = this.querySelector(".arrow");
 
-        if (panel.classList.contains("open")) {
-            panel.classList.remove("open");
-            arrow.style.transform = "rotate(-45deg)";
-        } else {
-            panel.classList.add("open");
-            arrow.style.transform = "rotate(45deg)"; 
-        }
-    });
+            if (panel.classList.contains("open")) {
+                panel.classList.remove("open");
+                arrow.style.transform = "rotate(-45deg)";
+            } else {
+                panel.classList.add("open");
+                arrow.style.transform = "rotate(45deg)"; 
+            }
+        });
+    }
 }
+
+Accordion();
